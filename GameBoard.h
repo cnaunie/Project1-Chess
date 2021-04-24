3b1195a54d510cfc64a0bd4aae8c68093b6895e0
cnaunie <56559856+cnaunie@users.noreply.github.com>
2021-04-24 12:51:06 -0700

    Update GameBoard.h

--- GameBoard.h
+++ GameBoard.h
@@ -15,3 +15,27 @@ using namespace std;
 			int x;
 			int y;
 		};
+
+class Board
+{
+	Square square[8][8];
+	Color turn=WHITE;
+	bool moveKing(Square* thisKing, Square* thatSpace);
+	bool moveQueen(Square* thisQueen, Square* thatSpace);
+	bool moveBishop(Square* thisBishop, Square* thatSpace);
+	bool moveKnight(Square* thisKnight, Square* thatSpace);
+	bool moveRook(Square* thisRook, Square* thatSpace);
+	bool movePawn(Square* thisPawn, Square* thatSpace);
+	bool makeMove(int x1, int y1, int x2, int y2);
+	void printBoard();
+	
+	
+public:
+		GameBoard(void);
+		~GameBoard(void);
+		struct positions {
+			int x;
+			int y;
+		};
+	
+	
